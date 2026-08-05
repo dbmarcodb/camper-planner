b74b93ee90a343e03e5650c4b69e9545205d38b1
dbmarcodb <acdbmarco@gmail.com>
2026-08-05 09:54:19 +0200

    Initialize map and add route button functionality

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-console.log("Camper Planner avviato");
+const map = L.map('map').setView([45.4384, 10.9916], 6);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
+
+
+let marker = L.marker([45.4384, 10.9916])
+    .addTo(map)
+    .bindPopup("Camper Planner")
+    .openPopup();
+
+
+document.getElementById("routeButton").addEventListener("click", function(){
+
+    const start = document.getElementById("start").value;
+    const waypoint = document.getElementById("waypoint").value;
+    const end = document.getElementById("end").value;
+
+    alert(
+        "Partenza: " + start +
+        "\nTappa: " + waypoint +
+        "\nArrivo: " + end
+    );
+
+});
