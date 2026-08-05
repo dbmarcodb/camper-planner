bcdfe35694599e758f0d3395abb82614a6ae1b1c
dbmarcodb <acdbmarco@gmail.com>
2026-08-05 14:34:29 +0200

    Refactor map initialization and route drawing functions

--- js/map.js
+++ js/map.js
@@ -1,37 +1,219 @@
 let map;
+
 let routeLayer;
 
-function initMap() {
+let markers = [];
 
-    map = L.map('map').setView([45.4384, 10.9916], 6);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '© OpenStreetMap contributors'
-    }).addTo(map);
+
+
+
+function initMap(){
+
+
+    map = L.map("map");
+
+
+    L.tileLayer(
+
+        "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+
+        {
+
+            attribution:
+            "&copy; OpenStreetMap contributors"
+
+        }
+
+    ).addTo(map);
+
+
+
+    map.setView(
+        [45.4642, 9.1900],
+        6
+    );
+
 
 }
 
 
-function drawRoute(coordinates) {
 
-    if (routeLayer) {
-        map.removeLayer(routeLayer);
+
+
+
+
+function clearMap(){
+
+
+    if(routeLayer){
+
+        map.removeLayer(
+            routeLayer
+        );
+
     }
 
-    routeLayer = L.polyline(coordinates, {
-        weight: 5
-    }).addTo(map);
 
 
-    map.fitBounds(routeLayer.getBounds());
+    markers.forEach(
+        function(marker){
+
+            map.removeLayer(marker);
+
+        }
+    );
+
+
+
+    markers = [];
+
 
 }
 
 
-function addMarker(lat, lon, text) {
 
-    L.marker([lat, lon])
-        .addTo(map)
-        .bindPopup(text);
+
+
+
+
+
+
+function drawRoute(
+    coordinates
+){
+
+
+    clearMap();
+
+
+
+    const latlngs =
+
+    coordinates.map(
+        function(point){
+
+            return [
+
+                point[1],
+
+                point[0]
+
+            ];
+
+        }
+
+    );
+
+
+
+    routeLayer =
+
+    L.polyline(
+
+        latlngs,
+
+        {
+
+            weight: 5
+
+        }
+
+    ).addTo(map);
+
+
+
+    map.fitBounds(
+        routeLayer.getBounds()
+    );
+
+
+}
+
+
+
+
+
+
+
+
+
+function addMarker(
+    lat,
+    lon,
+    text
+){
+
+
+    const marker =
+
+    L.marker(
+
+        [
+
+            lat,
+
+            lon
+
+        ]
+
+    )
+
+    .addTo(map)
+
+    .bindPopup(text);
+
+
+
+    markers.push(marker);
+
+
+
+}
+
+
+
+
+
+
+
+
+
+function addStopMarker(
+    lat,
+    lon
+){
+
+
+    const marker =
+
+    L.marker(
+
+        [
+
+            lat,
+
+            lon
+
+        ]
+
+    )
+
+    .addTo(map);
+
+
+
+    marker.bindPopup(
+
+        "<b>Sosta prevista</b><br>" +
+
+        "Punto stimato"
+
+    );
+
+
+
+    markers.push(marker);
+
 
 }
